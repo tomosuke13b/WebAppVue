d80d501b924922cf541618066ffcf7b211f55994
tomosuke <tomosuke13b@gmail.com>
2020-07-12 01:06:09 +0900

    下部にバーを設置

---
+++ WebAppVue/front/src/components/bottom/BottomBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-bottom-navigation
+    style="bottom:0px"
+    fixed
+    horizontal
+  >
+    <v-btn value="home" to="/">
+      <span>Home</span>
+      <v-icon>fa-home</v-icon>
+    </v-btn>
+
+    <v-btn value="list" to="/test/list">
+      <span>List</span>
+      <v-icon>fa-list</v-icon>
+    </v-btn>
+
+    <v-btn value="nearby">
+      <span>Nearby</span>
+      <v-icon>fa-map-marker</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+</template>
+
+<script>
+
+    export default {
+        name: "BottomBar",
+
+        props: {
+        },
+        data: () => ({
+        }),
+        computed: {
+        },
+        mounted() {
+        },
+        methods: {
+        }
+    }
+</script>
